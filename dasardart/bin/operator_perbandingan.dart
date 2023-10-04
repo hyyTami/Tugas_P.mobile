2510e10af2468bfd26b9627a7a5a71a6c98e953e
hyyTami <muhammadhaitamibjm@gmail.com>
2023-10-04 14:16:01 -0700

    OP_aritmatika_edit

--- dasardart/bin/operator_perbandingan.dart
+++ dasardart/bin/operator_perbandingan.dart
@@ -1,3 +1,11 @@
 void main() {
-  
+  int data1 = 17;
+  int data2 = 5;
+  print("$data1 == $data2 | ${data1==data2}");
+  print("$data1 != $data2 | ${data1!=data2}");
+  print("$data1 < $data2 | ${data1<data2}");
+  print("$data1 > $data2 | ${data1>data2}");
+  print("$data1 <= $data2 | ${data1<=data2}");
+  print("$data1 >= $data2 | ${data1>=data2}");
+ 
 }
