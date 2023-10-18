ecbf116b9442a6848f9c0099f5a0c04fb44cca96
hyyTami <muhammadhaitamibjm@gmail.com>
2023-10-17 20:24:18 -0700

    edit_cascade

--- dasardart/bin/OOP_tugas/Constructor/casecade_notation.dart
+++ dasardart/bin/OOP_tugas/Constructor/casecade_notation.dart
@@ -15,9 +15,17 @@ void main(){
       ..username = "eko"
       ..name = "Eko"
       ..email = "eko@contoh.com";
+      print(user.username);
+      print(user.name);
+      print(user.email);
+
 
   User? user2 = createUser()
     ?..username = "eko"
     ..name = "Eko"
     ..email = "eko@contoh.com";
+      print(user2?.username);
+      print(user2?.name);
+      print(user2?.email);
+
 }
